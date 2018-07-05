401671a92db39f4c395575644601d1b7a42fad4d
migtomace <32564100+migtomace@users.noreply.github.com>
2018-07-05 01:57:42 -0700

    commits app with current location

--- app/src/main/java/com/websitesinseattle/locationex/MapsActivity.java
+++ app/src/main/java/com/websitesinseattle/locationex/MapsActivity.java
@@ -1,14 +1,28 @@
 package com.websitesinseattle.locationex;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.sip.SipSession;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,6 +36,15 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private BroadcastReceiver broadcastReceiver;
+    private double currLat;
+    private double currLong;
+    private LatLng latLng;
+    private LocationManager locationManager;
+    private LocationListener listener;
+
+
+
     private GoogleMap mMap;
 
     @Override
@@ -32,8 +55,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                //get current location AND
+                //create a LatLng for camera animation
+                currLong = location.getLongitude();
+                currLat = location.getLatitude();
+                latLng = new LatLng(currLat, currLong);
+
+                //animate camera with current location
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+
+                //after camera is animated remove location updates
+                locationManager.removeUpdates(this);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        get_location();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                get_location();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void get_location(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+
+                //noinspection MissingPermission
+        //sets up location updates frequency
+                locationManager.requestLocationUpdates("gps", 100, 0, listener);
+    }
 
     /**
      * Manipulates the map once available.
@@ -76,12 +165,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         meetingsArray.add(meeting4);
 
 
-
         for (Meeting meeting : meetingsArray ){
-            // Add a marker in Sydney and move the camera
+            // add a marker for each meeting in the meetings array
             LatLng marker = new LatLng(getLats(meeting.getAddress()), getLongs(meeting.getAddress()));
             mMap.addMarker(new MarkerOptions().position(marker).title(meeting.getName()));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED) {
 
@@ -141,4 +228,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         return longs;
     }
+
+
 }
